ee547c5ac4a2530dbc2e7f497a1f4feaceeec06e
milanarif <milanarif@gmail.com>
2021-10-06 14:38:34 +0200

    sharpe optimization added

--- ex1.py
+++ ex1.py
@@ -51,7 +51,7 @@ def constrained_mv_weights(data, start, end, intervals):
     return np.around(np.array(weights), 4)
 
 
-def param_weights(returns, bm, start, end):
+def param_weights(thetas, returns, bm, start, end):
     weights = []
     # TODO: ONLY ONE MONTH?
     # TODO: 120 or 121 ??
@@ -65,7 +65,7 @@ def param_weights(returns, bm, start, end):
 
         row_weights = []
         for x in range(10):
-            row_weights.append(round((0.1 * (1 + (0.9 * std_return[x]) + (0.8 * std_bm[x]))), 4))
+            row_weights.append((0.1 * (1 + (thetas[0] * std_return[x]) + (thetas[1] * std_bm[x]))))
         weights.append(row_weights)
 
     return np.asarray(weights)
@@ -118,7 +118,7 @@ def portfolio_returns(weights, returns):
 
 w1 = mv_weights(returns_sheet, 0, returns_sheet.shape[0], 120)
 w2 = constrained_mv_weights(returns_sheet, 0, returns_sheet.shape[0], 120)
-w3 = param_weights(returns_sheet, BM_sheet, 120, returns_sheet.shape[0])
+w3 = param_weights([0.9, 0.8], returns_sheet, BM_sheet, 120, returns_sheet.shape[0])
 w4 = constrained_param_weights(w3)
 
 returns_p1 = portfolio_returns(w1, returns_sheet)
@@ -138,27 +138,49 @@ returns_p4 = portfolio_returns(w4, returns_sheet)
 
 # print((np.power((1 + np.mean(returns_p1)), 12) - 1) / (np.std(returns_p1)*np.sqrt(12)))
 # print((np.power((1 + np.mean(returns_p2)), 12) - 1) / (np.std(returns_p2)*np.sqrt(12)))
-# print((np.power((1 + np.mean(returns_p3)), 12) - 1) / (np.std(returns_p3)*np.sqrt(12)))
+print((np.power((1 + np.mean(returns_p3)), 12) - 1) / (np.std(returns_p3)*np.sqrt(12)))
 # print((np.power((1 + np.mean(returns_p4)), 12) - 1) / (np.std(returns_p4)*np.sqrt(12)))
 
 # QUESTION 3
-cumulative_returns_p1 = np.cumsum(returns_p1)
-cumulative_returns_p2 = np.cumsum(returns_p2)
-cumulative_returns_p3 = np.cumsum(returns_p3)
-cumulative_returns_p4 = np.cumsum(returns_p4)
-#
-plt.plot(cumulative_returns_p1, label="p1")
-plt.plot(cumulative_returns_p2, label="p2")
-plt.plot(cumulative_returns_p3, label="p3")
-plt.plot(cumulative_returns_p4, label="p4")
-plt.legend()
-plt.show()
+# cumulative_returns_p1 = np.cumsum(returns_p1)
+# cumulative_returns_p2 = np.cumsum(returns_p2)
+# cumulative_returns_p3 = np.cumsum(returns_p3)
+# cumulative_returns_p4 = np.cumsum(returns_p4)
+# #
+# plt.plot(cumulative_returns_p1, label="p1")
+# plt.plot(cumulative_returns_p2, label="p2")
+# plt.plot(cumulative_returns_p3, label="p3")
+# plt.plot(cumulative_returns_p4, label="p4")
+# plt.legend()
+# plt.show()
 
 
 # QUESTION 4
 
 # QUESTION 5
-# def max_p3_sr():
-#     w3 = param_weights(returns_sheet, BM_sheet, 120, returns_sheet.shape[0])
-#     portfolio_returns(w3, returns_sheet)
-#     (np.power((1 + np.mean(returns_p3)), 12) - 1) / (np.std(returns_p3) * np.sqrt(12))
+def max_p3_sr(returns, bm, start, end):
+
+    def min_objective(thetas, returns, bm, start, end):
+        w = param_weights(thetas, returns, bm, start, end)
+        r = portfolio_returns(w, returns)
+
+        return -(np.power((1 + np.mean(r)), 12) - 1) / (np.std(r) * np.sqrt(12))
+
+    def optimization(returns, bm, start, end):
+        x0 = np.array([0.75, 0.75])
+        bounds = [(0, 1.5), (0, 1.5)]
+
+        maximum = optimize.minimize(min_objective, x0, args=(returns, bm, start, end), method="SLSQP", bounds=bounds)
+
+        opt = maximum.x
+
+        max_sharpe = round(-maximum.fun, 4)
+        theta_1 = round(float(opt[0]), 4)
+        theta_2 = round(float(opt[1]), 4)
+
+        return [max_sharpe, theta_1, theta_2]
+
+    return optimization(returns, bm, start, end)
+
+
+print(max_p3_sr(returns_sheet, BM_sheet, 120, returns_sheet.shape[0]))
